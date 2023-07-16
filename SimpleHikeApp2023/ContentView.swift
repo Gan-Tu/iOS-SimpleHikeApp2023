53a3cb671dd10a96eaeeb2bb79e2596b788b35fe
Gan Tu <github@tugan.dev>
2023-07-16 14:55:22 -0400

    Initial Commit

---
+++ SimpleHikeApp2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleHikeApp2023
+//
+//  Created by Gan Tu on 7/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleHikeApp2023/SimpleHikeApp2023App.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleHikeApp2023App.swift
+//  SimpleHikeApp2023
+//
+//  Created by Gan Tu on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleHikeApp2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
